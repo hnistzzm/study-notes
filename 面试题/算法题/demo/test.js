e197669e8c2b9f1f01e2034a26d15254173c4a9d
zzm <957957209@qq.com>
2022-03-25 14:54:51 +0800

    3.225

--- 面试题/算法题/demo/test.js
+++ 面试题/算法题/demo/test.js
@@ -1,13 +1,52 @@
-var cuttingRope = function(n) {
-    if(n<=3) return n-1;
-    let a = parseInt(n / 3) ;
-    let rem = n % 3;
-    if(rem === 0){
-        return 3 ** a;
-    }else if (rem === 1){
-        return 2 * 2 * (3 ** (a-1));
-    }else{
-        return 2 * (3 ** a);
-    }
+/**
+ * initialize your data structure here.
+ */
+ var MinStack = function() {
+    this.minin = Number.MAX_SAFE_INTEGER;
+    // this.topin = null;
+    this.stk = [];
+
+    return this.stk;
 };
-console.log(cuttingRope(10)); 
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    
+    this.minin = (this.minin < x) ? this.minin : x;
+
+    this.stk.push(x);
+    console.log("push",this.stk);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    console.log("pop",this.stk);
+    this.stk.length--;
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    console.log("top",this.stk);
+    // return this.stk[this.stk.length-1] || null;
+    return 1;
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.min = function() {
+    return this.minin;
+};
+ var obj = new MinStack()
+  obj.push(5)
+  obj.pop()
+//   var param_3 = obj.top()
+//   var param_4 = obj.min()
+  console.log(obj)
